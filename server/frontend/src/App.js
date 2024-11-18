9b6a4e4ef5ebfd4e0bc13f6c66adc7fdd26fd279
SamDevl <R.samuel3@hotmail.com@example.com>
2024-11-18 12:50:21 -0500

    Dealerships Review function Update

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,8 @@ import LoginPanel from "./components/Login/Login";
 import RegistrationPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
  
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/Register" element={<RegistrationPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
+          <Route path="/dealer/:id" element={<Dealer/>} />
+                <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
